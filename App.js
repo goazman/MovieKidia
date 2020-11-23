677cfc1765cb8692ebecad6c1fa66deb156a05ec
Julien Le Goaziou <goazman@hotmail.com>
2020-11-23 21:26:49 +0100

    stackNavigator

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Search from "./components/Search";
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack  = createStackNavigator();
+
+function NavStack() {
+  return(
+    <Stack.Navigator headerMode= "screen" initialRouteName='Search'>
+      <Stack.Screen name="Search" component={Search} options={{ title: "Rechercher des films"}}/>
+    </Stack.Navigator>
+  );
+}
 
 export default function App (){
-
-    return (
-      <Search/>
-    )
+  return (
+    <NavigationContainer>
+      <NavStack/>
+    </NavigationContainer>
+  );
 }
 
 
